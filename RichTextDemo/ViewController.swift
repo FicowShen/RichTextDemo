8f86a2866675ab55eb6451c4e782071bb3b25a65
Ficow Shen <ficowshen@hotmail.com>
2019-11-24 19:41:03 +0800

    Initial Commit

---
+++ RichTextDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RichTextDemo
+//
+//  Created by Ficow on 2019/11/24.
+//  Copyright © 2019 ficow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
